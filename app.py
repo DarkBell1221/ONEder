2d03d5a0a941f3f129658294bfd5202e8b5d0042
DarkBell1221 <belldark74@gmail.com>
2024-09-09 23:36:21 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<p>Hola, Mundo!</p>"
+
+@app.route("/alumnos")
+def alumnos():
+    return render_template("alumnos.html")
+
+@app.route("/alumnos/guardar", methods=["POST"])
+def alumnosGuardar():
+
+    matricula    =request.form["txtMatriculaFA"]
+    nombreapellido    =request.form["txtNombreApellidoFA"]
+    return f"matricula: {matricula} Nombre y Apellido: {nombreapellido}"
